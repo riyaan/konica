12a82a0a5c90fb8260b1825a5ab762b83025921d
Riyaan Williams <riyaan.williams@gmail.com>
2013-10-30 13:59:36 +0200

    .

---
+++ MobileSAPIntegrationService/Tests/Configuration.Tests/ManagerUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common.Configuration;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class ManagerUnitTest
+    {
+        [TestMethod]
+        public void GetConfigSettingsSuccess()
+        {
+            Manager manager = new Manager();
+            SapConnection sapConnection = manager.GetSapConfigurationInformation();
+
+            Assert.AreNotEqual(null, sapConnection.Host);
+        }
+    }
+}
